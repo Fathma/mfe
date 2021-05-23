6ee68c274ff3b9b2356f31207a2ce3c51fc73662
Fathma <siddiquefathma@gmail.com>
2021-05-23 16:55:27 +0600

    created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,6 +1,6 @@
 const { merge } = require("webpack-merge");
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const commonConfig = require("./webpack.common");
@@ -15,10 +15,6 @@ const devConfig = {
     },
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
