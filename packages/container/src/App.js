8de55695c41c8ff319a1001128adea80daccfb41
Fathma <siddiquefathma@gmail.com>
2021-05-29 00:26:19 +0600

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import MarketingShow from "./components/MarketingShow";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
 const App = () => {
   return (
-    <div>
-      <h1>hi !!!!!!!!!!!</h1>
-      <MarketingShow />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingShow />
+      </div>
+    </BrowserRouter>
   );
 };
 
