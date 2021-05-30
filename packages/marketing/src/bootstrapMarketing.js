400bbc8b863cdae9ff9ceff2ad8e92134e1f2417
Fathma <siddiquefathma@gmail.com>
2021-05-31 01:18:07 +0600

    router communication btween container and marketin

--- packages/marketing/src/bootstrapMarketing.js
+++ packages/marketing/src/bootstrapMarketing.js
@@ -3,15 +3,27 @@ import ReactDom from "react-dom";
 import App from "./App";
 import { createMemoryHistory } from "history";
 
-const mount = (el) => {
+const mount = (el, { onNavigate }) => {
   const history = createMemoryHistory();
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
   ReactDom.render(<App history={history} />, el);
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathName) {
+        history.push(nextPathName);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV == "development") {
   const dev_root = document.querySelector("#_marketing_dev_root");
   if (dev_root) {
-    mount(dev_root);
+    mount(dev_root, {});
   }
 }
 export { mount };
